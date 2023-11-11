9691c09eaddc8c63028ce6c4386d9859d804434a
yulia-bisyuk <yulia.bisyuk@gmail.com>
2023-11-11 17:41:32 +0200

    feat: vacant area gallery updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,99 +1,112 @@
 import React from 'react';
-import {NavbarWrapper} from './styles';
+import { NavbarWrapper } from './styles';
 import useOutsideAlerter from '../../hooks/useOutsideAlerter';
-import {useTranslation} from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
-function Navbar({changeLanguage}) {
-  const {t, i18n} = useTranslation();
-  const {refForOutside, isShowed, setIsShowed} = useOutsideAlerter(false);
+function Navbar({ changeLanguage }) {
+  const { t, i18n } = useTranslation();
+  const { refForOutside, isShowed, setIsShowed } = useOutsideAlerter(false);
   return (
     <NavbarWrapper>
-      <div id="navbar" className="navbar-area navbar-style-two" ref={refForOutside}>
-        <div className="tuam-nav">
-          <div className="container-fluid">
-            <nav className="navbar navbar-expand-md navbar-light">
-              <a className="navbar-brand" href="/">
+      <div
+        id='navbar'
+        className='navbar-area navbar-style-two'
+        ref={refForOutside}
+      >
+        <div className='tuam-nav'>
+          <div className='container-fluid'>
+            <nav className='navbar navbar-expand-md navbar-light'>
+              <a className='navbar-brand' href='/'>
                 <img
-                  src="/images/white-logo.png"
-                  alt="logo"
-                  className="white-logo"
+                  src='/images/white-logo.png'
+                  alt='logo'
+                  className='white-logo'
                 />
-                <img src="/images/logo.png" alt="logo" className="logo"/>
+                <img src='/images/logo.png' alt='logo' className='logo' />
               </a>
-              <div className="call-us-top">
-                <div className="call-us-top-icon-text-wrap">
-                  <div className="icon">
-                    <i className="flaticon-call"/>
+              <div className='call-us-top'>
+                <div className='call-us-top-icon-text-wrap'>
+                  <div className='icon'>
+                    <i className='flaticon-call' />
                   </div>
-                  <span className="text">{t('contact_us')}:</span>
+                  <span className='text'>{t('contact_us')}:</span>
                 </div>
-                <span className="number">
-                  <a href="tel:+38 097 080-30-22">+38 097 080 30 22</a>
+                <span className='number'>
+                  <a href='tel:+38 097 080-30-22'>+38 097 080 30 22</a>
                 </span>
               </div>
               <a
-                onClick={() => changeLanguage(i18n.language === 'en' ? 'ua' : 'en')}
-                className="default-btn small-scr"
-                style={{cursor: 'pointer'}}
+                onClick={() =>
+                  changeLanguage(i18n.language === 'en' ? 'ua' : 'en')
+                }
+                className='default-btn small-scr'
+                style={{ cursor: 'pointer' }}
               >
                 {i18n.language === 'en' ? 'ua' : 'en'}
-                <span/>
+                <span />
               </a>
               <button
                 className={`navbar-toggler navbar-toggler-right ${
                   !isShowed ? 'collapsed' : ''
                 }`}
                 onClick={() => setIsShowed(!isShowed)}
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
+                type='button'
+                data-toggle='collapse'
+                data-target='#navbarSupportedContent'
+                aria-controls='navbarSupportedContent'
+                aria-expanded='false'
+                aria-label='Toggle navigation'
               >
-                <span className="icon-bar top-bar"/>
-                <span className="icon-bar middle-bar"/>
-                <span className="icon-bar bottom-bar"/>
+                <span className='icon-bar top-bar' />
+                <span className='icon-bar middle-bar' />
+                <span className='icon-bar bottom-bar' />
               </button>
               <div
                 className={`collapse navbar-collapse ${isShowed ? 'show' : ''}`}
-                id="navbarSupportedContent"
+                id='navbarSupportedContent'
               >
                 <ul
-                  className="navbar-nav"
+                  className='navbar-nav'
                   onClick={() => setIsShowed(!isShowed)}
                 >
-                  <li className="nav-item">
-                    <a className="nav-link" href="#advantages">
+                  <li className='nav-item'>
+                    <a className='nav-link' href='#advantages'>
                       {t('about_business_center')}
                     </a>
                   </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#gallery">
+                  <li className='nav-item'>
+                    <a className='nav-link' href='#gallery'>
                       {t('gallery')}
                     </a>
                   </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#contacts">
+                  <li className='nav-item'>
+                    <a className='nav-link' href='#contacts'>
                       {t('contacts')}
                     </a>
                   </li>
                 </ul>
-                <div className="others-option">
+                <div className='others-option'>
                   <a
-                    className="default-btn nav-presentation-btn"
-                    href="/files/arena-city.pdf"
+                    className='default-btn nav-presentation-btn'
+                    // href="/files/arena-city.pdf"
+                    href={
+                      i18n.language === 'en'
+                        ? 'files/Arena-City-11-2023-EN.pdf'
+                        : 'files/Арена-Сіті-11-2023.pdf'
+                    }
                     download
-                    target="_blank"
+                    target='_blank'
                   >
-                    {t('download_presentation')} <span/>
+                    {t('download_presentation')} <span />
                   </a>
                 </div>
               </div>
               <a
-                onClick={() => changeLanguage(i18n.language === 'en' ? 'ua' : 'en')}
-                className="default-btn large-scr"
-                style={{cursor: 'pointer'}}
+                onClick={() =>
+                  changeLanguage(i18n.language === 'en' ? 'ua' : 'en')
+                }
+                className='default-btn large-scr'
+                style={{ cursor: 'pointer' }}
               >
                 {i18n.language === 'en' ? 'ua' : 'en'}
                 <span />
--- src/components/office-plans/OfficePlans.jsx
+++ src/components/office-plans/OfficePlans.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import useMediaQuery from '../../hooks/useMediaQuery';
 import PlansCarousel from './PlansCarousel';
-import {useTranslation} from 'react-i18next';
-import {PlansWrap} from './styles';
-import {tabs_ua, tabs_en, floors} from '../../data/floors';
+import { useTranslation } from 'react-i18next';
+import { PlansWrap } from './styles';
+import { tabs_ua, tabs_en, floors } from '../../data/floors';
 
 function OfficePlans() {
   const [activeTab, setActiveTab] = useState(0);
   const [tabsContent, setTabsContent] = useState([]);
   const isMobile = useMediaQuery('(max-width: 991px)');
-  const {t, i18n} = useTranslation();
+  const { t, i18n } = useTranslation();
   const tabs = i18n.language === 'en' ? tabs_en : tabs_ua;
-  
+
   useEffect(() => {
     setTabsContent(Array.from(document.querySelectorAll('#tab')));
   }, [isMobile]);
-  
+
   useEffect(() => {
     tabsContent.map((tab, index) => {
       return index === activeTab
@@ -24,7 +24,7 @@ function OfficePlans() {
         : (tab.className = 'tabs_item hidden');
     });
   }, [activeTab, tabsContent]);
-  
+
   const getFloorButtons = () => {
     return tabs.map((tab, index) => (
       <li
@@ -36,34 +36,36 @@ function OfficePlans() {
       >
         {tab}
       </li>
-    ))
-  }
-  
+    ));
+  };
+
   return (
     <PlansWrap>
-      <section className="floor-plans-area ptb-100">
-        <div className="container">
-          <div className="section-title">
+      <section className='floor-plans-area ptb-100'>
+        <div className='container'>
+          <div className='section-title'>
             <h2>{t('officePlans.title')}</h2>
             <p>{t('officePlans.description')}</p>
           </div>
-          
-          <div className="row">
-            <div className="col-lg-12 col-md-12">
-              <div className="tab">
-                <ul className="tabs">
-                  {getFloorButtons()}
-                </ul>
-                <div className="tab_content">
-                  <div id="tab">
-                    <div className="row align-items-center">
-                      <div className="col-lg-6 col-md-12 content">
-                        <div className="tabs_item_content">
+
+          <div className='row'>
+            <div className='col-lg-12 col-md-12'>
+              <div className='tab'>
+                <ul className='tabs'>{getFloorButtons()}</ul>
+                <div className='tab_content'>
+                  <div id='tab'>
+                    <div className='row align-items-center'>
+                      <div className='col-lg-6 col-md-12 content'>
+                        <div className='tabs_item_content'>
                           <h3>{t('officePlans.arenaCity')}</h3>
                           <p>{t('officePlans.arenaCityDescription')}</p>
-                          <ul className="features-list">
+                          <ul className='features-list'>
                             <li>
-                              {t('officePlans.totalArea')} <span>14 165 {i18n.language === 'en' ? 'm' : 'м'}<sup>2</sup></span>
+                              {t('officePlans.totalArea')}{' '}
+                              <span>
+                                14 165 {i18n.language === 'en' ? 'm' : 'м'}
+                                <sup>2</sup>
+                              </span>
                             </li>
                             <li>
                               {t('officePlans.floors')} <span>1-6</span>
@@ -71,61 +73,77 @@ function OfficePlans() {
                             <li>
                               {t('officePlans.lifts')} <span>9</span>
                             </li>
-                            
+
                             <li>
-                              {t('officePlans.smallestOffice')} <span>35 {i18n.language === 'en' ? 'm' : 'м'}<sup>2</sup></span>
+                              {t('officePlans.smallestOffice')}{' '}
+                              <span>
+                                35 {i18n.language === 'en' ? 'm' : 'м'}
+                                <sup>2</sup>
+                              </span>
                             </li>
                             <li>
-                              {t('officePlans.largestOffice')} <span>2500 {i18n.language === 'en' ? 'm' : 'м'}<sup>2</sup></span>
+                              {t('officePlans.largestOffice')}{' '}
+                              <span>
+                                2500 {i18n.language === 'en' ? 'm' : 'м'}
+                                <sup>2</sup>
+                              </span>
                             </li>
                           </ul>
                           <a
-                            className="default-btn"
-                            href="/files/arena-city.pdf"
+                            className='default-btn'
+                            href={
+                              i18n.language === 'en'
+                                ? 'files/Arena-City-11-2023-EN.pdf'
+                                : 'files/Арена-Сіті-11-2023.pdf'
+                            }
                             download
-                            target="_blank"
+                            target='_blank'
                           >
-                            {t('download_presentation')} <span/>
+                            {t('download_presentation')} <span />
                           </a>
                         </div>
                       </div>
-                      <div className="col-lg-6 col-md-12 image">
-                        <div className="tabs_item_image">
+                      <div className='col-lg-6 col-md-12 image'>
+                        <div className='tabs_item_image'>
                           <img
-                            src="./images/floorPlans/1.jpeg"
-                            alt="floor-img"
-                            className="first"
+                            src='./images/floorPlans/1.jpeg'
+                            alt='floor-img'
+                            className='first'
                           />
                         </div>
                       </div>
                     </div>
                   </div>
-                  
+
                   {floors.map((floor, index) => {
                     return isMobile ? (
                       // mobile
-                      <div id="tab" className="tabs_item" key={index}>
-                        <div className="row align-items-center">
-                          <div className="col-lg-6 col-md-12 content">
-                            <div className="tabs_item_content">
+                      <div id='tab' className='tabs_item' key={index}>
+                        <div className='row align-items-center'>
+                          <div className='col-lg-6 col-md-12 content'>
+                            <div className='tabs_item_content'>
                               <h3>{tabs[index + 1].toUpperCase()}</h3>
-                              <div className="col-lg-6 col-md-12 image">
-                                <div className="tabs_item_image">
+                              <div className='col-lg-6 col-md-12 image'>
+                                <div className='tabs_item_image'>
                                   <img
                                     src={floor.plan}
-                                    alt="floor-plan"
-                                    style={{marginBottom: '20px'}}
+                                    alt='floor-plan'
+                                    style={{ marginBottom: '20px' }}
                                   />
                                 </div>
                               </div>
-                              <PlansCarousel images={floor.images}/>
+                              <PlansCarousel images={floor.images} />
                               <a
-                                className="default-btn"
-                                href="/files/arena-city.pdf"
+                                className='default-btn'
+                                href={
+                                  i18n.language === 'en'
+                                    ? 'files/Arena-City-11-2023-EN.pdf'
+                                    : 'files/Арена-Сіті-11-2023.pdf'
+                                }
                                 download
-                                target="_blank"
+                                target='_blank'
                               >
-                                {t('download_presentation')} <span/>
+                                {t('download_presentation')} <span />
                               </a>
                             </div>
                           </div>
@@ -133,28 +151,32 @@ function OfficePlans() {
                       </div>
                     ) : (
                       // desktop
-                      <div id="tab" className="tabs_item" key={index}>
-                        <div className="row align-items-center">
-                          <div className="col-lg-6 col-md-12 image">
-                            <div className="tabs_item_image">
-                              <img src={floor.plan} alt="floor-plan"/>
+                      <div id='tab' className='tabs_item' key={index}>
+                        <div className='row align-items-center'>
+                          <div className='col-lg-6 col-md-12 image'>
+                            <div className='tabs_item_image'>
+                              <img src={floor.plan} alt='floor-plan' />
                             </div>
                           </div>
-                          <div className="col-lg-6 col-md-12 content">
-                            <div className="tabs_item_content">
+                          <div className='col-lg-6 col-md-12 content'>
+                            <div className='tabs_item_content'>
                               <h3>{tabs[index + 1].toUpperCase()}</h3>
                               <PlansCarousel
                                 images={floor.images}
                                 key={index}
                               />
-                              
+
                               <a
-                                className="default-btn"
-                                href="/files/arena-city.pdf"
+                                className='default-btn'
+                                href={
+                                  i18n.language === 'en'
+                                    ? 'files/Arena-City-11-2023-EN.pdf'
+                                    : 'files/Арена-Сіті-11-2023.pdf'
+                                }
                                 download
-                                target="_blank"
+                                target='_blank'
                               >
-                                {t('download_presentation')} <span/>
+                                {t('download_presentation')} <span />
                               </a>
                             </div>
                           </div>
